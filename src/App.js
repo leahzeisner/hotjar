6c2d962327a670b3b7ae56b6a36de8aa6ba5b27d
leahzeisner <leahzeisner@gmail.com>
2025-05-29 13:45:24 -0400

    trigger survey

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const handleClick = () => {
+    if (window.hj) {
+      window.hj("trigger", "show_ms_recommendation_survey"); // Use your actual Hotjar trigger name
+    } else {
+      console.warn("Hotjar not loaded yet");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Hotjar Test!</p>
+        <button onClick={handleClick}>
+          Not interested in this recommended project
+        </button>
       </header>
     </div>
   );
